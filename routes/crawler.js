cbab57e0c85486f8c2bc1b3b2a3fbbb5a1398bee
syjsmk <syjsmk@naver.com>
2015-04-21 01:45:39 +0900

    make downloads folder and make folder under the downloads using article title

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,13 +1,63 @@
 var http = require('http');
 var exec = require('child_process').exec;
 var htmlparser = require('htmlparser2');
+
+var fs = require('fs');
+var util = require('util');
+
+// var hasUnicode = require("has-unicode");
+
+// if(hasUnicode()) {
+//     console.log("has Unicode");
+// }
+
+var isTagOpen = false;
+var articleTitle = '';
 var parser = new htmlparser.Parser({
+
     onopentag: function(name, attributes) {
-	if(name === "h1" && attributes.class === "entry_title") {
-	    console.log('attributes.class : ' + attributes.class);
+	// each site has different type of title -_-
+	// if(name === "h1" && attributes.class === "entry_title") {
+	if(name === 'title') {
+	    isTagOpen = true;
+	    // console.log('attributes.id : ' + attributes.id);
 	}
+    },
+
+    ontext: function(text) {
+	if(isTagOpen === true) {
+	    articleTitle = text;
+	    var modifiedArticleTitle = articleTitle.replace(/\s\W/gi, '');
+	    
+	    console.log('article Title : ' + articleTitle);
+	    console.log('modified title : ' + modifiedArticleTitle);
+	    
+
+	    // var child = exec('mkdir ./downloads', function(error, stdout, stderr) {
+    	    // 	console.log('stdout : ' + stdout);
+    	    // 	console.log('stderr : ' + stderr);
+    	    // 	if(error != null) {
+    	    // 	    console.log('exec error : ' + error);
+    	    // 	};
+	    // });
+
+
+	    var child = exec('mkdir -p ./downloads/' + modifiedArticleTitle, function(error, stdout, stderr) {
+    	    	    console.log('stdout : ' + stdout);
+    	    	    console.log('stderr : ' + stderr);
+    	    	    if(error != null) {
+    	    		console.log('exec error : ' + error);
+    	    	    };
+	    	});
+	    
+	    isTagOpen = false;
+	}
+
     }
 });
+
+// parser.write("qqqq<title>asdasd</title>ttttt");
+// parser.end();
 
 
 // TODO: url may become text + url (from mt2)
@@ -16,6 +66,7 @@ var crawling = function(url) {
 	// console.log('url in crawling');
 	// console.log(url);    
 
+    // TODO: update regex for other site.
     var regex = /http[^\s]*jpg/gi;
     
 
@@ -29,36 +80,44 @@ var crawling = function(url) {
 
     	var body = '';
 	
-    	console.log('got response: ' + res.statusCode);
-
-    	// console.log(res);
+    	// console.log('got response: ' + res.statusCode);
 
     	res.on('data', function(chunk) {
+	    // console.log(chunk);
     	    body += chunk;
     	});
     	res.on('end', function() {
     	    // console.log(body);
 
-	    // parser.write(body);
-	    // parser.end();
+	    // comment: write broken hiragana to file. body is ok.
+	    // var log_file = fs.createWriteStream(__dirname + '/body.html', {flags : 'w'});
+	    // var log_stdout = process.stdout;
+	    // log_file.write(util.format(body) + '\n');
+	    // log_stdout.write(util.format(body) + '\n');
+
+	    // var body = "aaqqqq<title>asdasd</title>ttttt";
+	    parser.write(body);
+	    parser.end();
 
 	    var matches_array = body.match(regex);
-	    console.log(matches_array);
+	    // console.log(matches_array);
 
 	    // TODO: cannot use foreach?
 	    //for(var extractedUrl in matches_array) {
+	    
+	    // write picture files using wget
 	    for(var i = 0; i < matches_array.length; i ++) {
-		console.log(matches_array[i]);
+		// console.log(matches_array[i]);
 		
 		//		console.log(extractedUrl);
 
-		var child = exec('wget -P downloads ' + matches_array[i], function(error, stdout, stderr) {
-    		    // console.log('stdout : ' + stdout);
-    		    // console.log('stderr : ' + stderr);
-    		    if(error != null) {
-    			console.log('exec error : ' + error);
-    		    };
-		});
+		// var child = exec('wget -P downloads ' + matches_array[i], function(error, stdout, stderr) {
+    		//     // console.log('stdout : ' + stdout);
+    		//     // console.log('stderr : ' + stderr);
+    		//     if(error != null) {
+    		// 	console.log('exec error : ' + error);
+    		//     };
+		// });
 
 	    }
 
